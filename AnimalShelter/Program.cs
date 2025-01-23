a642964195753fdf6dbc67553a04ed114efd8f86
Kamil-Pawelski <kp02224@gmail.com>
2025-01-23 19:35:21 +0100

    Initial project + db connection

---
+++ AnimalShelter/Program.cs
@@ -0,0 +1,33 @@
+﻿using AnimalShelter.Domain;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); // TODO: Move to Configuration
+});
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
